eb3f1698ffd5e932503cb6b9e54d30cad72887d9
velutinx <velutinx@yahoo.com>
2026-02-25 10:11:45 -0700

    Delete test-fetch.js

--- test-fetch.js
+++
@@ -1,4 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(res => res.json())
-  .then(data => console.log('TEST SUCCESS:', data))
-  .catch(err => console.error('TEST FAILED:', err.message));
