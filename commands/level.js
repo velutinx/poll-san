81d3c6e08cd67545767a742174342563777d53be
velutinx <velutinx@yahoo.com>
2026-04-26 02:51:33 -0700

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -1,14 +1,13 @@
-// this is poll-san/commands/level.js
-
+// commands/level.js
 const { EmbedBuilder, MessageFlags } = require('discord.js');
 const supabase = require('../services/supabase');
-const { colors } = require('../utils/helpers');
+const h = require('../utils/helpers');
 
 module.exports = async (interaction) => {
     const targetUser = interaction.isUserContextMenuCommand() ? interaction.targetUser : interaction.user;
     await interaction.deferReply({ flags: [MessageFlags.Ephemeral] });
 
-    const { data } = await supabase.from('user_xp')
+    const { data } = await supabase.from(h.tables.USER_XP)
         .select('*')
         .eq('user_id', targetUser.id)
         .eq('guild_id', interaction.guildId)
@@ -18,7 +17,7 @@ module.exports = async (interaction) => {
 
     const levelEmbed = new EmbedBuilder()
         .setTitle(`${data.discord_username}'s Stats`)
-        .setColor(colors.success)
+        .setColor(h.colors.success)
         .addFields(
             { name: 'Level', value: `⭐ ${data.level}`, inline: true },
             { name: 'Messages', value: `💬 ${data.total_messages}`, inline: true }
