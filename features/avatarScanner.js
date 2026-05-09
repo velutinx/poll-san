e878964f7d68c8c438c0e69810469d477120ec3e
velutinx <velutinx@yahoo.com>
2026-05-09 06:49:33 -0700

    Update avatarScanner.js

--- features/avatarScanner.js
+++ features/avatarScanner.js
@@ -687,7 +687,7 @@ function init(client) {
     client.on(Events.UserUpdate, onUserUpdate);
 
     client.once(Events.ClientReady, () => {
-        console.log('[AvatarScan] Bot ready. Mass scan will start in 30 seconds...');
+  //      console.log('[AvatarScan] Bot ready. Mass scan will start in 30 seconds...');
         setTimeout(() => {
             scanAllMembersWithFreeAPI(client).catch(err => console.error('[MassScan] Error:', err));
         }, 30000);
