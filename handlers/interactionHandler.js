0ef98ac897c6aaa121c57aeb3449974ed7484dc5
velutinx <velutinx@yahoo.com>
2026-05-08 18:12:56 -0700

    Update interactionHandler.js

--- handlers/interactionHandler.js
+++ handlers/interactionHandler.js
@@ -16,11 +16,10 @@ const {
     handleSuggestModalSubmit
 } = require('../services/redeemHandler');
 
-const checkinSessions = new Map(); // userId -> { interaction, messageId, timestamp }
+const checkinSessions = new Map();
 
 module.exports = async function handleInteraction(interaction) {
     try {
-        // Chat input commands
         if (interaction.isChatInputCommand()) {
             switch (interaction.commandName) {
                 case 'level': require('../commands/level')(interaction); break;
@@ -36,71 +35,48 @@ module.exports = async function handleInteraction(interaction) {
                 default: break;
             }
         }
-        // User context menu
         else if (interaction.isUserContextMenuCommand() && interaction.commandName === 'View Level') {
             require('../commands/level')(interaction);
         }
-        // Buttons
         else if (interaction.isButton()) {
-            if (interaction.customId === 'shop_buy_confirm') {
-                await handleShopPurchase(interaction);
-            }
-            else if (interaction.customId === 'slots_bet_1') {
-                await handleSlotsBet(interaction, 1);
-            }
-            else if (interaction.customId === 'slots_bet_5') {
-                await handleSlotsBet(interaction, 5);
-            }
-            else if (interaction.customId === 'slots_bet_25') {
-                await handleSlotsBet(interaction, 25);
-            }
-            else if (interaction.customId === 'hangman_start_button') {
-                await startHangmanGame(interaction);
-            }
-            else if (interaction.customId === 'verify_start') {
-                await handleVerifyStart(interaction);
-            }
-            else if (interaction.customId === 'checkin_claim') {
-                await handleCheckinClaim(interaction);
-            }
-            else if (interaction.customId === 'cointoss_bet_1') {
-                await handleCoinTossBet(interaction, 1);
-            }
-            else if (interaction.customId === 'cointoss_bet_5') {
-                await handleCoinTossBet(interaction, 5);
-            }
-            else if (interaction.customId === 'cointoss_bet_25') {
-                await handleCoinTossBet(interaction, 25);
-            }
-            // ========== Ticket Store buttons ==========
-            else if (interaction.customId === 'redeem_start') {
-                await handleRedeemStart(interaction);
-            }
-            else if (interaction.customId.startsWith('redeem_series_')) {
-                const index = parseInt(interaction.customId.split('_')[2]);
-                await handleRedeemSeries(interaction, index);
-            }
-            else if (interaction.customId === 'redeem_cancel') {
-                await handleRedeemCancel(interaction);
-            }
-            else if (interaction.customId === 'redeem_vote_power') {
-                await handleRedeemVoteBoost(interaction);
-            }
-            else if (interaction.customId === 'redeem_suggest_character') {
-                await handleRedeemSuggestCharacter(interaction);
-            }
-            // ===========================================
-            else {
-                await giveawayCommand.handleGiveawayButton(interaction);
+            switch (interaction.customId) {
+                // Shop
+                case 'shop_buy_confirm': await handleShopPurchase(interaction); break;
+                // Slots
+                case 'slots_bet_1': await handleSlotsBet(interaction, 1); break;
+                case 'slots_bet_5': await handleSlotsBet(interaction, 5); break;
+                case 'slots_bet_25': await handleSlotsBet(interaction, 25); break;
+                // Hangman
+                case 'hangman_start_button': await startHangmanGame(interaction); break;
+                // Verify
+                case 'verify_start': await handleVerifyStart(interaction); break;
+                // Checkin
+                case 'checkin_claim': await handleCheckinClaim(interaction); break;
+                // Coin Toss
+                case 'cointoss_bet_1': await handleCoinTossBet(interaction, 1); break;
+                case 'cointoss_bet_5': await handleCoinTossBet(interaction, 5); break;
+                case 'cointoss_bet_25': await handleCoinTossBet(interaction, 25); break;
+                // ----- Redeem / Ticket Store -----
+                case 'redeem_start': await handleRedeemStart(interaction); break;
+                case 'redeem_vote_power': await handleRedeemVoteBoost(interaction); break;
+                case 'redeem_suggest_character': await handleRedeemSuggestCharacter(interaction); break;
+                case 'redeem_cancel': await handleRedeemCancel(interaction); break;
+                default: {
+                    // Series selection buttons for character request
+                    if (interaction.customId.startsWith('redeem_series_')) {
+                        const index = parseInt(interaction.customId.split('_')[2]);
+                        await handleRedeemSeries(interaction, index);
+                    } else {
+                        await giveawayCommand.handleGiveawayButton(interaction);
+                    }
+                }
             }
         }
-        // Modals
         else if (interaction.isModalSubmit()) {
             if (interaction.customId === 'redeem_suggest_modal') {
                 await handleSuggestModalSubmit(interaction);
             }
         }
-        // Select menus
         else if (interaction.isStringSelectMenu() && interaction.customId === 'shop_select') {
             await handleShopSelect(interaction);
         }
