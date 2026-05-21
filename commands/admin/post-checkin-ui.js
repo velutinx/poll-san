89ab986e1772dfd6506c61233b671bff8a06fa21
velutinx <velutinx@yahoo.com>
2026-05-20 22:33:56 -0700

    Update post-checkin-ui.js

--- commands/admin/post-checkin-ui.js
+++ commands/admin/post-checkin-ui.js
@@ -1,6 +1,6 @@
 // commands/admin/post-checkin-ui.js
 
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, MessageFlags } = require('discord.js');
 const helpers = require('../../utils/helpers');
 
 module.exports = {
@@ -11,7 +11,7 @@ module.exports = {
         if (!interaction.memberPermissions.has('Administrator')) {
             return interaction.reply({
                 content: `${helpers.releaseEmojis.BATSU} Admin only.`,
-                ephemeral: true
+                flags: [MessageFlags.Ephemeral]
             });
         }
 
@@ -19,7 +19,7 @@ module.exports = {
         if (!checkinChannel) {
             return interaction.reply({
                 content: `${helpers.releaseEmojis.BATSU} Check-in channel not found.`,
-                ephemeral: true
+                flags: [MessageFlags.Ephemeral]
             });
         }
 
@@ -61,7 +61,7 @@ module.exports = {
 
         await interaction.reply({
             content: `${helpers.releaseEmojis.getRandomVerify()} Daily check-in message posted!`,
-            ephemeral: true
+            flags: [MessageFlags.Ephemeral]
         });
     }
 };
