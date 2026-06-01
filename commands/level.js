3d4d628e18de1f56a8656ac5d4e00ccb453bc22b
velutinx <velutinx@yahoo.com>
2026-06-01 13:53:28 -0700

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -1,17 +1,17 @@
 // commands/level.js
 const { EmbedBuilder, MessageFlags } = require('discord.js');
-const supabase = require('../services/supabase');
+const db = require('../services/database');
 const h = require('../utils/helpers');
 
 module.exports = async (interaction) => {
     const targetUser = interaction.isUserContextMenuCommand() ? interaction.targetUser : interaction.user;
     await interaction.deferReply({ flags: [MessageFlags.Ephemeral] });
 
-    const { data } = await supabase.from(h.tables.USER_XP)
-        .select('*')
-        .eq('user_id', targetUser.id)
-        .eq('guild_id', interaction.guildId)
-        .single();
+    const data = await db.query(
+        `SELECT * FROM ${h.tables.USER_XP} WHERE user_id = ? AND guild_id = ?`,
+        [targetUser.id, interaction.guildId],
+        true
+    );
 
     if (!data) return interaction.editReply({ content: "No stats found yet!" });
 
