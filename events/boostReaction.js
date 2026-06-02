fea9976efd23a6e2e463de9f4c30e42980983e9a
velutinx <velutinx@yahoo.com>
2026-06-02 07:29:48 -0700

    Update boostReaction.js

--- events/boostReaction.js
+++ events/boostReaction.js
@@ -1,13 +1,16 @@
-// events/boostReaction.js
 const { MessageType } = require('discord.js');
 const h = require('../utils/helpers');
 
 module.exports = async (message) => {
-    if (message.type === MessageType.UserPremiumGuildSubscription) {
+    const isBoost = message.type === MessageType.UserPremiumGuildSubscription ||
+                    message.type === MessageType.PremiumGuildSubscription;
+    if (isBoost) {
         try {
             const confettiEmoji = h.releaseEmojis?.CONFETTI || '🎉';
             await message.react(confettiEmoji);
+            console.log(`🎉 Reacted to boost message from ${message.author?.tag || 'Unknown'}`);
         } catch (err) {
+            console.warn('Failed to react to boost message:', err.message);
         }
     }
 };
