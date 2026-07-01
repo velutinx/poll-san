0add79de503b5ab225cd82eca204fd014e7a8c4d
velutinx <velutinx@yahoo.com>
2026-07-01 04:11:55 -0700

    Update r2Storage.js

--- services/r2Storage.js
+++ services/r2Storage.js
@@ -5,7 +5,9 @@ const R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID;
 const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID;
 const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY;
 const R2_BUCKET_NAME = process.env.R2_BUCKET_NAME || 'img';
+
 const PUBLIC_BASE_URL = process.env.R2_PUBLIC_URL || `https://${R2_BUCKET_NAME}.${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`;
+
 const r2Client = new S3Client({
     region: 'auto',
     endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
